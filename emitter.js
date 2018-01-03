49224faa20ed7519c880e8228031c4ded12dcd85
Kevin Lanzon <kevinlanzon@gmail.com>
2018-01-03 13:55:34 +0000

    Emit function

--- emitter.js
+++ emitter.js
@@ -8,3 +8,13 @@ Emitter.prototype.on = function (type, listener) {
   this.events[type] = this.events[type] || [];
   this.events[type].push(listener);
 };
+
+Emitter.prototype.emit = function (type) {
+  if (this.events[type]) {
+    this.events[type].forEach(function(listener){
+      listener();
+    });
+  }
+};
+
+module.exports = Emitter;
