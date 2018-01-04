f8d6230fd17a931f3ef94a0ee4cb76458f5cb904
Kevin Lanzon <kevinlanzon@gmail.com>
2018-01-04 14:25:56 +0000

    Switches Emitter to class syntax

--- emitter.js
+++ emitter.js
@@ -1,18 +1,20 @@
 'use strict';
 
-function Emitter() {
-  this.events = {};
-}
-
-Emitter.prototype.on = function (type, listener) {
-  this.events[type] = this.events[type] || [];
-  this.events[type].push(listener);
-};
-
-Emitter.prototype.emit = function (type) {
-  if (this.events[type]) {
-    this.events[type].forEach(listener => listener());
+class Emitter {
+  constructor() {
+    this.events = {};
   }
-};
+
+  on(type, listener) {
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(listener);
+  }
+
+  emit(type) {
+    if (this.events[type]) {
+      this.events[type].forEach(listener => listener());
+    }
+  }
+}
 
 module.exports = Emitter;
